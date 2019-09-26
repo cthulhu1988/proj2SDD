ec18ca9377eae6753c008e1924ccc7f2db10722f
Isaac.CallisonSD <isaacgcallison@protonmail.com>
2019-09-26 14:59:16 -0500

    working

--- p2.py
+++ p2.py
@@ -111,8 +111,10 @@ def main():
         csv_inputs = csv_row[:-1]
         csv_desired = int(csv_row[-1])
         one_hot = [0,0,0]
-        one_hot[csv_desired] = 1
 
+        # init onehot with desired output
+        one_hot[csv_desired] = 1
+        # renamed for repeated use 
         inputData = csv_inputs
 
         currentLayer = 0
@@ -153,66 +155,6 @@ def main():
                 hiddenLayerList.append(m)
 
 
-        deltaSumOutput = 0
-        one_h_count = 0
-        partialDerivList = []
-        for i in outputNeuronList:
-            error = (one_hot[one_h_count] - i.value)**2
-            print("error {}".format(error))
-            one_h_count +=1
-            for x in range(len(i.weightList)):
-                deltaSumOutput += i.weightList[x] * hiddenLayerList[x].value
-            primedSig = sigmoidPrime(deltaSumOutput)
-            primedSigErr = primedSig* error
-            partialDerivList.append(primedSigErr)
-            # set deltas of output neurons
-            i.delta = primedSigErr
-            deltaSumOutput = 0
-            #i.printStats()
-
-        halfEq = []
-        deltaHiddenSum = 0
-        for k in hiddenLayerList:
-            k.printStats()
-            for x in range(len(k.weightList)):
-                deltaHiddenSum += k.weightList[x] * csv_inputs[x]
-            halfEq.append(deltaHiddenSum)
-            deltaHiddenSum = 0
-
-        amt = 0
-        nLst = len(outputNeuronList) # 3
-        wLst = len(outputNeuronList[0].weightList)
-
-        lastHalfEq = []
-        for jj in range(wLst):
-            for y in range(nLst):
-                amt += outputNeuronList[y].delta * outputNeuronList[y].weightList[jj]
-            lastHalfEq.append(amt)
-            amt = 0
-
-        zipped =[a*b for a,b in zip(halfEq,lastHalfEq)]
-        zippedPrime = [ sigmoidPrime(a) for a in zipped ]
-        kkCount = 0
-        for kk in hiddenLayerList:
-            kk.delta = zippedPrime[kkCount]
-            kkCount+=1
-        kkCount = 0
-
-        print("Item in neuron list")
-        for item in outputNeuronList:
-            itemCount = 0
-            for i in range(len(item.weightList)):
-                print(item.weightList[i])
-                item.weightList[i] += item.weightList[i] * item.delta * hiddenLayerList[i].value *-.05
-                print(item.weightList[i])
-            print()
-
-        for itm in hiddenLayerList:
-            for i in range(len(itm.weightList)):
-                print(itm.weightList[i])
-                itm.weightList[i] = itm.weightList[i] * itm.delta * inputData[i]
-                print(itm.weightList[i])
-            print()
 
 
 # Normalize = number - smallest / the difference between the numbers
