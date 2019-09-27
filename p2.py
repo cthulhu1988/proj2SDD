df21fcfbae93ea8c56ccca1eb7a261bdb4c83b39
isaac.callisonSD <isaac.callisonsd@mtsu.edu>
2019-09-26 22:41:49 -0500

    training is working

--- p2.py
+++ p2.py
@@ -1,8 +1,68 @@
 #!/usr/bin/env python3
 import sys, math, csv
+import numpy as np
 
-def main():
-    test = "this now works"
+class NeuralNetwork:
+
+    def __init__(self, inputNodes,hiddenNodes,outputNodes ):
+        self.iNodes = inputNodes
+        self.oNodes = outputNodes
+        self.hNodes = hiddenNodes
+        self.wInputHidden = np.random.rand(self.hNodes,self.iNodes)
+        self.wOutput = np.random.rand(self.oNodes, self.hNodes)
+        self.lr = 1.0
+
+    def printStats(self):
+        print("number of inputs {}, hidden {}, output {}".format(self.iNodes, self.oNodes, self.hNodes))
+        print("wInputHidden")
+        print(self.wInputHidden)
+        print("wOutput")
+        print(self.wOutput)
+
+    def train(self, inputs_list, targets_list):
+        inputs = np.array(inputs_list, ndmin=2).T
+        targets = np.array(targets_list, ndmin=2).T
+        ############################# FIRST LAYER
+        hidden_inputs = np.dot(self.wInputHidden, inputs)
+        hidden_outputs = np.array([self.sigmoid(x) for x in hidden_inputs])
+        ############################ SECOND LAYER
+        final_inputs = np.dot(self.wOutput, hidden_outputs)
+        final_outputs = np.asarray([self.sigmoid(x) for x in final_inputs])
+        ################################ ERRORS
+        # Calculate ERRORS at the ouput NODES
+        output_errors = np.array(targets - final_outputs, ndmin=2)
+        ################## BackPropagation STEPS
+        outputErrorDerived = output_errors * final_outputs * (1.0 - final_outputs)
+        hidden_errors = np.dot(self.wOutput.T, output_errors)
+        ################## adjust the weights ########################
+        self.wOutput += self.lr * np.dot((outputErrorDerived), np.transpose(hidden_outputs))
+        self.wInputHidden += self.lr * np.dot((hidden_errors * hidden_outputs * (1.0 - hidden_outputs)), np.transpose(inputs))
+
+
+
+    def query(self, input_list):
+        # Create numpy array with input list
+        inputs = np.array(input_list).T
+
+        # calculate new hidden inputs from inputs and weights of input layer
+        hidden_inputs = (np.dot(self.wInputHidden, inputs))
+        # run middle layer through sigmoid
+        siged_hidden_outputs = [self.sigmoid(x) for x in hidden_inputs ]
+        # multiply outputs with output weights
+        final_inputs = np.dot(self.wOutput, siged_hidden_outputs)
+        # run last layer through sigmoid
+        siged_final_outputs = [self.sigmoid(x) for x in final_inputs]
+
+        return siged_final_outputs
+
+    def sigmoid(self, s):
+    # activation function
+        return 1/(1+np.exp(-s))
+
+    def sigmoidPrime(self, s):
+        #derivative of sigmoid
+        return s * (1 - s)
+
 
 class Neuron:
     def __init__(self, layer, neuron, weightList, bias):
@@ -68,11 +128,19 @@ def main():
             data = data.strip()
             listData = data.split()
             if len(listData)>0:
+                print(listData)
                 neuronList.append(NeuronMaker(listData))
-
-
             line = fp.readline()
+            for n in neuronList:
+                
+
     fp.close()
+
+    for n in neuronList:
+        print("stats")
+        n.printStats()
+
+
 
     # OPEN and PARSE CSV file ##########################
     with open(fileCSV, newline = '') as csvfile:
@@ -83,6 +151,8 @@ def main():
         for row in reader:
             rowleng = len(row) -1
             CSVinputList.append(row)
+
+
 
 ############## Normalize data in list ###################
         count = 0
@@ -102,110 +172,38 @@ def main():
             max = 0
             count +=1
 
-
+    newNN = NeuralNetwork(rowleng,5,3)
 
 ########### data normalized
+    for y in range(epochs):
+        for x in range(low_trn_rng, hi_trn_rng):
+            csv_row = CSVinputList[x]
+            csv_inputs = csv_row[:-1]
+            csv_desired = int(csv_row[-1])
 
-    for x in range(low_trn_rng, hi_trn_rng):
-        csv_row = CSVinputList[x]
-        csv_inputs = csv_row[:-1]
-        csv_desired = int(csv_row[-1])
-        one_hot = [0,0,0]
+            one_hot = [0,0,0]
 
-        # init onehot with desired output
-        one_hot[csv_desired] = 1
-        # renamed for repeated use 
-        inputData = csv_inputs
-
-        currentLayer = 0
-        newHiddenList=[]
-        sum = 0
-        for n in neuronList:
-            if n.layer == currentLayer:
-                for x in range(len(inputData)):
-                    sum += n.weightList[x] * inputData[x]
-                sum += n.bias
-                neuronalSum = sigmoid(sum)
-                n.value = neuronalSum
-                newHiddenList.append(neuronalSum)
-                sum = 0
+            # init onehot with desired output
+            if rowleng < 5:
+                one_hot[csv_desired] = 1
             else:
-                inputData = newHiddenList
-                newHiddenList = []
-                sum = 0
-                currentLayer +=1
-                for x in range(len(inputData)):
-                    sum += n.weightList[x] * inputData[x]
-                sum += n.bias
-                neuronalSum = sigmoid(sum)
-                n.value = neuronalSum
-                newHiddenList.append(neuronalSum)
-                sum = 0
+                one_hot[csv_desired -1] = 1
+            # renamed for repeated use
+            inputData = np.array(csv_inputs)
+            #print("input Data")
+            #print(inputData)
 
-        outPutLayerNumber = currentLayer
-        #print("Final output newHiddenList: ", newHiddenList)
-        #neuronList = reversed(neuronList)
-        outputNeuronList = []
-        hiddenLayerList = []
-        # split up output and hidden
-        for m in neuronList:
-            if m.layer == outPutLayerNumber:
-                outputNeuronList.append(m)
-            else:
-                hiddenLayerList.append(m)
+            newNN.train(inputData, one_hot)
 
+    test = np.array([0.194, 0.541, 0.067, 0.041,0.1,0.1,0.1])
+    print(newNN.query(test))
 
-
-
-# Normalize = number - smallest / the difference between the numbers
-# so for 1 and 5 -> 1 -1 = 0 / d=4 = 0
-
-# alpha is the momentum and we will not use that
-# we will not adjust the biases.
-# The Learning rate N will be set to 1 so adding it is irrelevant
-# we need to code for two possibilities. First that we are in the hidden layer or layers and second the output nodes.
-
-# def backprop(network, expected):
-#     for i in reversed(range(len(network))
-#         layer = network[i]
-#         error = []
-#         if i != len(network) - 1:
-#             for j in range(len(layer)):
-#                 error = 0.0
-#                 for neuron in network[i+1]:
-#                     error += 2(neuron[w][j]) * neuron(dolab??)
-#                     append(??)
-#         else:
-#             for j in range(len(layer)):
-#                 neuron = layer[j]
-#                 error.append(expected[j] - neuron(???))
-#                 for j in range(len(layer)):
-#                     neuron = jay?[j]
-#
-# def expected(network, layer(or row??), learning_rate):
-#     for in in range len(layer):
-#         input = row
-#         if i != 0:
-#             input = neuron
-#         expected = [0 for i in range(num of rates??)]
 
 
 def normalized(smallest, largest, number):
     #print("smallest {} largest {} and number to normalize {}".format(smallest, largest, number))
     normalized = (number - smallest)/(largest - smallest)
     return normalized
-
-# derivative function
-def sigmoidPrime(x):
-    return (sigmoid(x) - (1-sigmoid(x)))
-
-
-# sigmoid activation function
-def sigmoid(x):
-    return 1 / (1 + math.exp(-x))
-
-
-
 
 
 if __name__ == '__main__':
