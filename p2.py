2af06d5cc26be637163d32abd86f9623ff1c0167
Isaac.CallisonSD <isaacgcallison@protonmail.com>
2019-09-27 16:04:15 -0500

    mostly done

--- p2.py
+++ p2.py
@@ -11,6 +11,7 @@ class NeuralNetwork:
         self.wInputHidden = np.random.rand(self.hNodes,self.iNodes)
         self.wOutput = np.random.rand(self.oNodes, self.hNodes)
         self.lr = 1.0
+        bias = 1.0
 
     def printStats(self):
         print("number of inputs {}, hidden {}, output {}".format(self.iNodes, self.oNodes, self.hNodes))
@@ -58,10 +59,6 @@ class NeuralNetwork:
     def sigmoid(self, s):
     # activation function
         return 1/(1+np.exp(-s))
-
-    def sigmoidPrime(self, s):
-        #derivative of sigmoid
-        return s * (1 - s)
 
 
 class Neuron:
@@ -128,17 +125,11 @@ def main():
             data = data.strip()
             listData = data.split()
             if len(listData)>0:
-                print(listData)
+                #print(listData)
                 neuronList.append(NeuronMaker(listData))
             line = fp.readline()
 
     fp.close()
-
-
-    for n in neuronList:
-        print("stats")
-        n.printStats()
-
 
 
     # OPEN and PARSE CSV file ##########################
@@ -150,7 +141,6 @@ def main():
         for row in reader:
             rowleng = len(row) -1
             CSVinputList.append(row)
-
 
 
 ############## Normalize data in list ###################
@@ -171,34 +161,112 @@ def main():
             max = 0
             count +=1
 
+    layer1 = []
+    layer2 = []
+    for n in neuronList:
+        if n.layer == 0:
+            layer1.append(n.weightList)
+        elif n.layer == 1:
+            layer2.append(n.weightList)
+    layer1 = np.array(layer1)
+    layer2 = np.array(layer2)
+
+
+
     newNN = NeuralNetwork(rowleng,5,3)
+    newNN.wInputHidden = layer1
+    newNN.wOutput = layer2
+    if(internals_flag) == 1:
+        newNN.printStats()
 
 ########### data normalized
-    for y in range(epochs):
+    for y in range(epochs+1):
+        epoch_count = 0
+        epoch_count_correct = 0
         for x in range(low_trn_rng, hi_trn_rng):
+            epoch_count += 1
+
             csv_row = CSVinputList[x]
             csv_inputs = csv_row[:-1]
             csv_desired = int(csv_row[-1])
 
             one_hot = [0,0,0]
+            one_hot_index = 9
 
             # init onehot with desired output
             if rowleng < 5:
                 one_hot[csv_desired] = 1
+                one_hot_index = csv_desired
             else:
                 one_hot[csv_desired -1] = 1
+                one_hot_index = (csv_desired - 1)
+            #print("One hot train")
+            #print(one_hot)
             # renamed for repeated use
             inputData = np.array(csv_inputs)
-            #print("input Data")
-            #print(inputData)
-
             newNN.train(inputData, one_hot)
-        if(y % 10 == 0):
-            print("Epoch :",y)
-            print(newNN.query(inputData))
+            test_nums = newNN.query(inputData)
 
+            index = 0
+            max = 0
+            for ii in range(len(test_nums)):
+                if test_nums[ii] > max:
+                    max = test_nums[ii]
+                    index = ii
+            if index == one_hot_index:
+                epoch_count_correct +=1
+        if y % 10 == 0:
+            if epoch_count_correct != 0:
+                percentage = epoch_count_correct / epoch_count
+                percentage = round(percentage, 4)
+            else:
+                percentage = 0
+            print("{}   {} correct of {}   {}".format(y,epoch_count_correct, epoch_count, percentage))
+            if(internals_flag) == 1:
+                newNN.printStats()
 
-    test = np.array([0.194, 0.541, 0.067, 0.041,0.1,0.1,0.1])
+    test_count = 0
+    test_count_correct = 0
+    for t in range(low_tst_rng, hi_tst_rng):
+        test_count += 1
+        csv_row = CSVinputList[t]
+        csv_inputs = csv_row[:-1]
+        csv_desired = int(csv_row[-1])
+        # print("csv row")
+        # print(csv_row)
+        one_hot_test = [0,0,0]
+        one_hot_index = 9
+
+        # init onehot with desired output
+        if rowleng < 5:
+            one_hot_test[csv_desired] = 1
+            one_hot_index = csv_desired
+        else:
+            one_hot_test[csv_desired -1] = 1
+            one_hot_index = (csv_desired - 1)
+
+        # renamed for repeated use
+        inputDataTest = np.array(csv_inputs)
+        # print("input data test")
+        # print(inputDataTest)
+        test_nums = newNN.query(inputDataTest)
+        # print("Test Nums")
+        # print(test_nums)
+        # print("output one hot")
+        # print(one_hot_test)
+        # print("desired index")
+        # print(one_hot_index)
+        index_test = 0
+        max_test = 0
+        for jj in range(len(test_nums)):
+            if test_nums[jj] > max:
+                max_test = test_nums[jj]
+                index_test = jj
+        if index_test == one_hot_index:
+            test_count_correct += 1
+        percentage = test_count_correct / test_count
+    print("Test {} correct of {}   {}".format(test_count_correct, test_count, percentage))
+    print()
 
 
 
